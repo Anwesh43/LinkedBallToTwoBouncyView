4000a9bc2b165bc9d3a589064ea522cd4e463655
Anwesh43 <anweshthecool0@gmail.com>
2020-03-14 13:42:23 +0530

    adidng view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedballtotwobouncyview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedballtotwobouncyview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedballtotwobouncyview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.balltotwobouncyview.BallToTwoBouncyView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallToTwoBouncyView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
