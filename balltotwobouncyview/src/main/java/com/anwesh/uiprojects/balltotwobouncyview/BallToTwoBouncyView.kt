c473922e9d8670c9f1adcdcaa2d709ddfc434faf
Anwesh43 <anweshthecool0@gmail.com>
2020-03-14 12:59:13 +0530

    adding constants and libraries

--- balltotwobouncyview/src/main/java/com/anwesh/uiprojects/balltotwobouncyview/BallToTwoBouncyView.kt
+++ balltotwobouncyview/src/main/java/com/anwesh/uiprojects/balltotwobouncyview/BallToTwoBouncyView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.balltotwobouncyview
 /**
  * Created by anweshmishra on 14/03/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodeColors : Array<String> = arrayOf("#673AB7", "#F44336", "#009688", "#8BC34A", "#FF9800")
+val circles : Int = 2
+val scGap : Float = 0.02f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 30
+val sizeFactor : Float = 4f
